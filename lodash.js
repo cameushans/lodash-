eabf58a6fe886d9ca3b683d38f3892195821dd6f
Hans <51888571+cameushans@users.noreply.github.com>
2020-12-09 11:45:16 +0100

    functionnal programming solution

--- lodash.js
+++ lodash.js
@@ -1,28 +1,26 @@
-
-const arrayToPush = ["a","b","c","d"]
-
+const arrayToPush = ["a","b","c","d"];
 //the function have 2 parameter , an array , and a number ;
-//implementing the chunk method of lodash library
+//implementing the chunk method of lodash library;
 
-function chunk (array,number){
+function chunk (array,number)  {
 
+    //no side effects on this function 
     const newArray = [];
-  
-    const  splitElement = () =>  array.map(element => element.split(""));
+    
+    const  splitElement = () =>  array.map(element => element.split(""));//point free style ? 
 
     const  sliceElementFirstElement = () =>  splitElement().slice(0,number);
     const sliceElementSecondElement = () => splitElement().slice(number);
 
     const joinElementOne= () =>   sliceElementFirstElement().join("");
     const joinElementTwo = () => sliceElementSecondElement().join('');
-  
+
     const resplitedElementOne = () => joinElementOne().split("");
-    const resplitedElementTwo = () => joinElementTwo().split("")
+    const resplitedElementTwo = () => joinElementTwo().split("");
 
-    newArray.push(resplitedElementOne());
-    newArray.push(resplitedElementTwo());
+    newArray.push(resplitedElementOne(),resplitedElementTwo());
 
-    console.log(newArray)
+    console.log(newArray);
 };
 
-chunk(arrayToPush,2)
+chunk(arrayToPush,3);

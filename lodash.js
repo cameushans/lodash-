393e80f814d1c401e38c2b214f881e2796593a46
Hans <51888571+cameushans@users.noreply.github.com>
2020-12-09 19:12:22 +0100

    simple as possible

--- lodash.js
+++ lodash.js
@@ -1,26 +1,16 @@
 const arrayToPush = ["a","b","c","d"];
-//the function have 2 parameter , an array , and a number ;
 //implementing the chunk method of lodash library;
 
 function chunk (array,number)  {
-
     //no side effects on this function 
     const newArray = [];
-    
-    const  splitElement = () =>  array.map(element => element.split(""));//point free style ? 
 
-    const  sliceElementFirstElement = () =>  splitElement().slice(0,number);
-    const sliceElementSecondElement = () => splitElement().slice(number);
+    const  sliceElementFirstElement = () =>  array.slice(0,number);
+    const sliceElementSecondElement = () => array.slice(number);
 
-    const joinElementOne= () =>   sliceElementFirstElement().join("");
-    const joinElementTwo = () => sliceElementSecondElement().join('');
+    newArray.push(sliceElementFirstElement(),sliceElementSecondElement())
+  
+      console.log(newArray);
+  };
 
-    const resplitedElementOne = () => joinElementOne().split("");
-    const resplitedElementTwo = () => joinElementTwo().split("");
-
-    newArray.push(resplitedElementOne(),resplitedElementTwo());
-
-    console.log(newArray);
-};
-
-chunk(arrayToPush,3);
+chunk(arrayToPush,2);
